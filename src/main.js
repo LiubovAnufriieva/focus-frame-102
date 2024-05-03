31e9009c9a178b2512cc8c658768c6133247d2bb
LiubovAnufriieva <lyuzika@gmail.com>
2024-05-03 15:53:22 +0300

    clear input added

--- src/main.js
+++ src/main.js
@@ -16,12 +16,22 @@
   const goBtn = document.querySelector('.footer-btn');
 
   goBtn.addEventListener('click', handleClick);
+  
+  const inputEmailFooter = document.querySelector(".footer-input");
 
   function handleClick(event) {
+
     event.preventDefault();
     if (event.target.classList.contains("footer-btn"))  {
+        clearInput();
         return alert('You successfully subscribed on our updates! Thank you!');
   } 
-  setTimeout(handleClick, 5000);
+  setTimeout(handleClick, 2000);
+ 
 }
   
+function clearInput() {
+    inputEmailFooter.value = ''; // Встановлюємо значення поля введення як порожній рядок
+}
+// Викликати функцію clearInput() для очищення поля введення
+
