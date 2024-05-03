701e0b1e0623e0b58917ec00e6584b014c1c2442
LiubovAnufriieva <lyuzika@gmail.com>
2024-05-03 14:29:36 +0300

    js file upd

--- src/main.js
+++ src/main.js
@@ -12,3 +12,16 @@
       refs.menu.classList.toggle("is-open");
     }
   })();
+
+  const goBtn = document.querySelector('.footer-btn');
+
+  goBtn.addEventListener('click', handleClick);
+
+  function handleClick(event) {
+    event.preventDefault();
+    if (event.target.classList.contains("footer-btn"))  {
+        return alert('You successfully subscribed on our updates! Thank you!');
+  } 
+  setTimeout(handleClick, 5000);
+}
+  
