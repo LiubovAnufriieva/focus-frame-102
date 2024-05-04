bc5da5fcdf146c28aceb0e141a279f6543af4645
LiubovAnufriieva <lyuzika@gmail.com>
2024-05-04 10:32:51 +0300

    not finished editing of js code onSubmit

--- src/main.js
+++ src/main.js
@@ -1,53 +1,61 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import 'modern-normalize';
 
 // Show message on submit subscription form in footer
-  const goBtn = document.querySelector('.footer-btn');
+const form = document.querySelector('.footer-form');
 
-  goBtn.addEventListener('click', handleClick);
-  
-  const inputEmailFooter = document.querySelector(".footer-input"); 
+form.addEventListener('submit', handleSubmit);
+
+const inputEmailFooter = document.querySelector('.footer-input');
 
 
-  function handleClick(event) {
- 
-    event.preventDefault();
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const userEmail = inputEmailFooter.value.trim();
+
+  if (userEmail || userEmail.checkValidity()) {
+    clearInput();
+
+    return iziToast.info({
+      animateInside: true,
+      position: 'topCenter',
+      title: 'Info',
+      message: 'You successfully subscribed on our updates! Thank you',
+    });
+  } else {
+    return iziToast.warning({
+      animateInside: true,
+      position: 'topCenter',
+      title: 'Info',
+      message: 'You put incorrect value of email!',
+    });
     
-    if (inputEmailFooter.value) { 
-      if (event.target.classList.contains("footer-btn"))  {
-
-        clearInput();
-  
-        return  iziToast.info({
-          animateInside: true,
-          position: 'topCenter',
-          title: 'Info',
-          message: 'You successfully subscribed on our updates! Thank you',
-        });; 
-  } 
-  setTimeout(handleClick, 2000);
-    }
- 
+  }
 }
+setTimeout(handleSubmit, 7000);
+
 // Встановлюємо значення поля введення як порожній рядок
 function clearInput() {
-    inputEmailFooter.value = ''; 
+  inputEmailFooter.value = '';
 }
-
 
 // Open and close the modal window in header
 
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector("[data-menu-open]"),
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
-    refs.menu.classList.toggle("is-open");
+    refs.menu.classList.toggle('is-open');
   }
 })();
